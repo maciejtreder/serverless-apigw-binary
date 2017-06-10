2fdb3ff5b0b0bda90ed247fc02cd2c8ccacd9fa8
maciejtreder <contact@maciejtreder.com>
2017-06-10 18:30:54 +0200

    adding nodejs example

---
+++ examples/express/lambda.js
@@ -0,0 +1,12 @@
+'use strict';
+const awsServerlessExpress = require('aws-serverless-express');
+const app = require('./dist/app');
+const binaryMimeTypes = [
+  'image/gif',
+  'text/css',
+  'text/html'
+]
+const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes)
+
+
+module.exports.express = (event, context) => awsServerlessExpress.proxy(server, event, context);
